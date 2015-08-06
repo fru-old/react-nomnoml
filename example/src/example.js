e82251ec6be36e74627f52e6093530eae5aaf2a4
Florian Rüberg <florian.rueberg@gmail.com>
2015-08-06 18:26:10 +0200

    Build component

--- example/src/example.js
+++ example/src/example.js
@@ -5,7 +5,48 @@ var App = React.createClass({
 	render () {
 		return (
 			<div>
-				<Nomnoml />
+				<Nomnoml source="
+
+
+[Pirate
+|eyeCount: Int
+|raid();pillage()
+|
+|[beard]--[parrot] 
+|[beard]-:>[foul mouth]
+];
+[<abstract>Marauder]<:--[Pirate]
+[<start>MarauderT]->[Pirate]
+[<instance> Marauder2]->[Pirate]
+[<note> Marauder3]->[Pirate]
+[<package> Marauder4]->[Pirate]
+[<frame> Marauder5]->[Pirate]
+[<database> Marauder6]->[Pirate]
+[<start> Marauder7]->[Pirate]
+[<end> Marauder8]->[Pirate]
+[<state> Marauder9]->[Pirate]
+[<choice> Marauder10]->[Pirate]
+[<input> Marauder11]->[Pirate]
+[<sender> Marauder12]->[Pirate]
+[<receiver> Marauder13]->[Pirate]
+[<actor> Marauder14]->[Pirate]
+[<usecase> Marauder15]->[Pirate]
+[<hidden> Marauder16]->[Pirate]
+[Pirate]* - 0..7[mischief]
+[jollyness]+-+[Pirate]
+[jollyness]--[rum]
+[jollyness]->[singing]
+[Pirate]-> *[rum|tastiness: Int|swig()]
+[Pirate]->[singing]
+[singing]<->[rum]
+[<start>st]->[<state>plunder]
+[plunder]->[<choice>more loot]
+[more loot]->[st]
+[more loot] no ->[<end>e]
+[<actor>Sailor] - [<usecase>shiver me;timbers]
+
+
+				"/>
 			</div>
 		);
 	}
--- src/Nomnoml.js
+++ src/Nomnoml.js
@@ -1,11 +1,18 @@
 var React = require('react');
+var graph = require('nomnoml');
 
 var Nomnoml = React.createClass({
-	
+	componentDidMount () {
+		var source = this.props.source || '';
+		//source = source.replace(/(\r\n|\r|\n){2,}/g, '\n');
+		console.log(source);
+		source = source.replace(/(\r|\n)(\t|\s|\r|\n)*/g, '\n');
+		console.log(source);
+		graph.draw(React.findDOMNode(this), source);
+	},
 	render () {
-		return <div>nomnoml2</div>;
+		return <canvas></canvas>;
 	}
-	
 });
 
 export default Nomnoml;
